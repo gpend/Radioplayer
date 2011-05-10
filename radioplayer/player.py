06e4db63334169fc348b51231218113f4d7083a7
Philippe Normand <phil@base-art.net>
2011-05-10 09:40:28 +0200

    removed mmkeys dependency

--- radioplayer/player.py
+++ radioplayer/player.py
@@ -2,15 +2,14 @@
 import pygst
 pygst.require('0.10')
 import gst
-
-import mmkeys
+import dbus
 
 class Player:
 
     def __init__(self, url, audiosink="autoaudiosink", output_location=None):
-        self.keys = mmkeys.MmKeys()
-        self.keys.connect("mm-playpause", self.playpause_cb)
-        self.keys.connect("mm-stop", self.stop)
+        bus = dbus.SessionBus()
+        bus.add_signal_receiver(self._key_pressed, signal_name="MediaPlayerKeyPressed",
+                                dbus_interface="org.gnome.SettingsDaemon.MediaKeys")
         if not output_location:
             self.pipeline = gst.element_factory_make("playbin2")
             self.pipeline.props.uri = url
@@ -37,13 +36,19 @@ class Player:
             self.stop()
             self.start()
 
+    def _key_pressed(self, foo, key):
+        if key == dbus.String(u"Play"):
+            self.toggle_play()
+        elif key == dbus.String(u"Stop"):
+            self.stop()
+
     def start(self):
         self.pipeline.set_state(gst.STATE_PLAYING)
 
     def stop(self, *args):
         self.pipeline.set_state(gst.STATE_NULL)
 
-    def playpause_cb(self, key, bar):
+    def toggle_play(self):
         result, state, pending = self.pipeline.get_state()
         if state == gst.STATE_PLAYING:
             new_state = gst.STATE_PAUSED
