3381516c7d5630c181d67567ddbb6ed53ec499ee
Philippe Normand <phil@base-art.net>
2012-03-11 17:16:54 +0100

    radios: use WebKit to get current FIP artist/title

--- radioplayer/radios.py
+++ radioplayer/radios.py
@@ -1,18 +1,32 @@
 
-import urllib2, BeautifulSoup
-import json
+from gi.repository import WebKit, GLib
 
 class FipRadio:
     live_url = "http://mp3.live.tv-radio.com/fip/all/fiphautdebit.mp3"
 
+    def __init__(self):
+        self._artist_song = ("", "")
+        self.web_view = WebKit.WebView()
+        self.web_view.connect("load-finished", self._load_finished_cb)
+        self.web_view.load_uri("http://www.fipradio.fr/")
+
+    def _inspect_data(self):
+        dom_document = self.web_view.get_dom_document()
+        elts = dom_document.get_elements_by_class_name("direct-item direct-distance-0 current")
+        elt = elts.item(0)
+        artiste_elt = elt.get_elements_by_class_name("artiste").item(0)
+        artiste = artiste_elt.get_inner_text().title()
+        titre_elt = elt.get_elements_by_class_name("titre").item(0)
+        titre = titre_elt.get_inner_text().title()
+        self._artist_song = (artiste, titre)
+        return True
+
+    def _load_finished_cb(self, view, frame):
+        self._inspect_data()
+        GLib.timeout_add_seconds(30, self._inspect_data)
+
     def artist_song(self):
-        data = urllib2.urlopen("http://fipradio.fr/sites/default/files/direct-large.json").read()
-        json_data = json.loads(data)
-        soup = BeautifulSoup.BeautifulSoup(json_data["html"])
-        div =  soup.findAll("div", attrs={"class":"direct-item direct-distance-0 current"})[0]
-        artiste = div.findAll("div", attrs={"class": "artiste"})[0].text
-        title = div.findAll("div", attrs={"class": "titre"})[0].text
-        return (artiste, title)
+        return self._artist_song
 
 class FranceInterRadio:
     live_url = "http://mp3.live.tv-radio.com/franceinter/all/franceinterhautdebit.mp3"
