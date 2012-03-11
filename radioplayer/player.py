d24034bfc8a0410d76171080b190e277e773044b
Philippe Normand <phil@base-art.net>
2012-03-11 18:27:31 +0100

    player: port to PyGI and GDBus

--- radioplayer/player.py
+++ radioplayer/player.py
@@ -1,8 +1,5 @@
 
-import pygst
-pygst.require('0.10')
-import gst
-import dbus
+from gi.repository import Gst, Gio, GLib
 
 class Player:
 
@@ -11,21 +8,28 @@ class Player:
         self._paused_or_stopped_cb = paused_or_stopped_cb
         self._app_name = "radioplayer"
 
-        bus = dbus.SessionBus()
-        remote_object = bus.get_object("org.gnome.SettingsDaemon", "/org/gnome/SettingsDaemon/MediaKeys")
-        self.proxy = dbus.Interface(remote_object, "org.gnome.SettingsDaemon.MediaKeys")
+        Gst.init([])
 
-        self.proxy.GrabMediaPlayerKeys(self._app_name, 0)
+        bus = Gio.bus_get_sync(Gio.BusType.SESSION, None)
+        self.proxy = Gio.DBusProxy.new_sync(bus, 0, None, "org.gnome.SettingsDaemon",
+                                            "/org/gnome/SettingsDaemon/MediaKeys",
+                                            "org.gnome.SettingsDaemon.MediaKeys", None)
 
-        bus.add_signal_receiver(self._key_pressed, signal_name="MediaPlayerKeyPressed",
-                                dbus_interface="org.gnome.SettingsDaemon.MediaKeys")
+        self.ping_gnome()
+
+        def on_signal(proxy, sender_name, signal_name, parameters):
+            if signal_name == "MediaPlayerKeyPressed":
+                self._key_pressed(*parameters)
+
+        self.proxy.connect("g-signal", on_signal)
+
         if not output_location:
-            self.pipeline = gst.element_factory_make("playbin2")
+            self.pipeline = Gst.ElementFactory.make("playbin2", "playbin")
             self.pipeline.props.uri = url
             if audiosink != "autoaudiosink":
-                self.pipeline.props.audio_sink = gst.element_factory_make(audiosink)
+                self.pipeline.props.audio_sink = Gst.ElementFactory.make(audiosink, "audiosink")
         else:
-            self.pipeline = gst.parse_launch("souphttpsrc location=%s "
+            self.pipeline = Gst.parse_launch("souphttpsrc location=%s "
                                              "! tee name=t queue t. ! decodebin2 "
                                              "! %s t. ! queue "
                                              "! filesink location=%s" % (url,
@@ -37,8 +41,10 @@ class Player:
         # TODO: send state notifications using signals
 
     def _on_gst_message(self, bus, message):
+        if not message:
+            return
         t = message.type
-        if t == gst.MESSAGE_ERROR:
+        if t == Gst.MessageType.ERROR:
             err, debug = message.parse_error()
             print "Error: %s" % err, debug
             print "Restarting..."
@@ -48,29 +54,30 @@ class Player:
     def _key_pressed(self, app, key):
         if app != self._app_name:
             return
-        if key == dbus.String(u"Play"):
+        if key == "Play":
             self.toggle_play()
-        elif key == dbus.String(u"Stop"):
+        elif key == "Stop":
             self.stop()
 
     def ping_gnome(self):
-        self.proxy.GrabMediaPlayerKeys(self._app_name, 0)
+        variant_args = GLib.Variant("(su)", (self._app_name, 0))
+        result  = self.proxy.call_sync("GrabMediaPlayerKeys", variant_args, 0, -1, None)
 
     def start(self):
-        self.pipeline.set_state(gst.STATE_PLAYING)
+        self.pipeline.set_state(Gst.State.PLAYING)
 
     def stop(self, *args, **kwargs):
         notify = kwargs.get("notify", True)
-        self.pipeline.set_state(gst.STATE_NULL)
+        self.pipeline.set_state(Gst.State.NULL)
         if notify and self._paused_or_stopped_cb:
             self._paused_or_stopped_cb()
 
     def toggle_play(self):
         result, state, pending = self.pipeline.get_state()
-        if state == gst.STATE_PLAYING:
-            new_state = gst.STATE_PAUSED
+        if state == Gst.State.PLAYING:
+            new_state = Gst.State.PAUSED
             if self._paused_or_stopped_cb:
                 self._paused_or_stopped_cb()
         else:
-            new_state = gst.STATE_PLAYING
+            new_state = Gst.State.PLAYING
         self.pipeline.set_state(new_state)
