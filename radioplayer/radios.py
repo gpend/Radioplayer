3a74910b8526709465c51a045a05f9ea9ce0db14
Philippe Normand <phil@base-art.net>
2012-03-11 18:26:53 +0100

    radios: reload FIP page when DOM goes zombie

--- radioplayer/radios.py
+++ radioplayer/radios.py
@@ -10,10 +10,16 @@ class FipRadio:
         self.web_view.connect("load-finished", self._load_finished_cb)
         self.web_view.load_uri("http://www.fipradio.fr/")
 
+    def reload(self):
+        self.web_view.load_uri("http://www.fipradio.fr/")
+
     def _inspect_data(self):
         dom_document = self.web_view.get_dom_document()
         elts = dom_document.get_elements_by_class_name("direct-item direct-distance-0 current")
         elt = elts.item(0)
+        if not elt:
+            self.reload()
+            return False
         artiste_elt = elt.get_elements_by_class_name("artiste").item(0)
         artiste = artiste_elt.get_inner_text().title()
         titre_elt = elt.get_elements_by_class_name("titre").item(0)
